e26b3c806c7e7ca26c48fcaaf44522198eda70b3
nhnam47 <125166623+nhnam47@users.noreply.github.com>
2023-02-19 17:39:51 +0700

    first commit

---
+++ src/test/java/com/uniq/iPet/IPetApplicationTests.java
@@ -0,0 +1,13 @@
+package com.uniq.iPet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IPetApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
